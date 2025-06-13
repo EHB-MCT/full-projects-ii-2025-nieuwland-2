cd84f1ed4c2f8ebde0d681e8b3dd22141410c318
Elouan Van Cappellen <elouan.van.cappellen@student.ehb.be>
2025-06-13 15:18:50 +0200

    extra schill animatie toegevoegd

--- js/cssRandomiser.js
+++ js/cssRandomiser.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // const styles = ['style1', 'style2', 'style3'];
-    const styles = ['style1']; //set style for testing
+    const styles = ['style1', 'style2', 'style3'];
+    // const styles = ['style2']; //set style for testing
     const randomStyle = styles[Math.floor(Math.random() * styles.length)];
 
     const composition = document.getElementById('imageComposition');
     if (composition) {
         composition.classList.add(randomStyle);
+        const images = composition.querySelectorAll('img');
+
+        images.forEach((img, index) => {
+            // Delay increases per image for build effect
+            setTimeout(() => {
+                img.classList.add('show');
+            }, 300 * index); // 300ms stagger
+        });
     }
 
     console.log("active style " + randomStyle);

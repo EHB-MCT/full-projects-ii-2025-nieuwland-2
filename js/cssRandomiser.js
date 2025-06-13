a782d54fd16c5fb6421573cd6252c98a1a971389
Elouan Van Cappellen <elouan.van.cappellen@student.ehb.be>
2025-06-13 09:30:22 +0200

    styling css2 done

--- js/cssRandomiser.js
+++ js/cssRandomiser.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const styles = ['style1', 'style2', 'style3'];
+    // const styles = ['style1', 'style2', 'style3'];
+    const styles = ['style3']; //set style for testing
     const randomStyle = styles[Math.floor(Math.random() * styles.length)];
 
     const composition = document.getElementById('imageComposition');
     if (composition) {
         composition.classList.add(randomStyle);
     }
+
+    console.log("active style " + randomStyle);
 });

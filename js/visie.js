879f40bc4a4df2f5ed48529ebf3484e51836a32d
Kenza Houben <kenza.tabti@student.ehb.be>
2025-06-15 18:09:33 +0200

    in progress

--- js/visie.js
+++ js/visie.js
@@ -71,6 +71,86 @@ function filter() {
 
     document.querySelector(".button-2").addEventListener('click', function () {
         console.log("This is button 2");
+        document.getElementById('content').innerHTML =
+            ` <div class="container">
+
+		<div class="orientatie-1">
+			<!-- <div class="o-title-1"> -->
+				<h2>Mens</h2>
+			<!-- </div> -->
+			<img src="./images/mens.png" class="mens">
+			<div class="o-vakje-1">
+				<p>Veel van onze kinderen groeien op in stressvolle omstandigheden en leren niet vanzelf hun emoties goed te uiten. Daarom investeren we als school extra in emotieregulatie en leren we kinderen de taal van emoties. Om schoolse achterstanden, demotivatie en uitval te voorkomen, versterken we het zelfbeeld en zelfvertrouwen van onze leerlingen. Via onze pedagogische en didactische keuzes willen we bovendien hun leerzin en motivatie blijven voeden.</p>
+			</div>
+		</div>
+
+		<div class="orientatie-2">
+			<div class="o-title-2">
+				<h2>Maatschappij</h2>
+			</div>
+			<img src="./images/maatschappij.png" class="maatschappij">
+			<div class="o-vakje-2">
+				<p>Omdat veel kinderen opgroeien in een omgeving met weinig actieve rolmodellen, brengen we hen in contact met diverse beroepen en inspirerende voorbeelden.Sociaal-cultureel: In onze superdiverse samenleving leren we kinderen samenleven in verscheidenheid, met aandacht voor zowel verschillen als gelijkenissen. We zien die diversiteit als een meerwaarde en benutten ze in ons aanbod.Politiek-juridisch: We geven kinderen inzicht in onze democratie en leren hen hoe ze kunnen deelnemen aan het maatschappelijk debat, zodat ze later actief kunnen opkomen voor hun rechten en belangen.</p>
+			</div>
+		</div>
+
+		<div class="orientatie-3">
+			<div class="o-title-3">
+				<h2>Ruimte: verkeer en mobiliteit</h2>
+			</div>
+			<div class="o-vakje-3">
+				<p>Door het drukke verkeer leggen we de nadruk op verkeersveiligheid, zodat kinderen zich veilig te voet en met de fiets kunnen verplaatsen. We maken optimaal gebruik van het openbaar vervoer bij uitstappen in het BHG, zodat kinderen leren zich zelfstandig te verplaatsen en hun leefwereld verbreden. Mobiliteit koppelen we aan milieueducatie, waarbij we hen bewust maken van hun ecologische impact en duurzame keuzes stimuleren.</p>
+			</div>
+		</div>
+
+		<div class="orientatie-4">
+			<img src="./images/tijd.png" class="tijd">
+			<div class="o-title-4">
+				<h2>Tijd</h2>
+			</div>
+			<div class="o-vakje-4">
+				<p>Om mee te kunnen in een snel veranderende samenleving, leren we kinderen hun dagelijks leven organiseren, beginnend bij tijdsbesef. Daarnaast laten we hen kennismaken met historische gebeurtenissen, zodat ze onze maatschappij beter begrijpen en bewuster over de toekomst kunnen nadenken.</p>
+			</div>
+		</div>
+
+		<div class="orientatie-5">
+			<img src="./images/levende_natuur.png" class="levende_natuur">
+			<div class="o-title-5">
+				<h2>Natuur: levende natuur</h2>
+			</div>
+			<div class="o-vakje-5">
+				<p>Onze kinderen groeien op in een verstedelijkt gebied en hebben vaak beperkte voorkennis over de natuur. Daarom laten we hen actief en ontdekkend kennismaken met diverse biotopen en organismen, binnen en buiten het BHG, zodat deze ervaringen blijven hangen.</p>
+			</div>
+		</div>
+
+		<div class="orientatie-6">
+			<div class="o-title-6">
+				<h2>Natuur: gezondheid</h2>
+			</div>
+			<div class="o-vakje-6">
+				<p>De levensomstandigheden van onze leerlingen houden verschillende gezondheidsrisico’s in (zowel fysiek als mentaal). Daarom is een doorgedreven gezondheidsbeleid op school van groot belang. Ook bewegingsopvoeding is hier een wezenlijk onderdeel van.</p>
+			</div>
+		</div>
+
+		<div class="orientatie-7">
+			<img src="./images/natuur_milieu.png" class="natuur_milieu">
+			<div class="o-title-7">
+				<h2>Natuur: Milieu</h2>
+			</div>
+			<div class="o-vakje-7">
+				<p>Kinderen zien in hun omgeving weinig voorbeelden van respectvol gedrag naar milieu. Zo wordt er bijvoorbeeld weinig gesorteerd en slingert er veel afval in de omliggende straten. We willen dat onze leerlingen opgroeien tot burgers die de wereld beter maken en dus is het belangrijk om vanuit de school voldoende klemtoon te leggen op milieu-educatie en ecologie.</p>
+			</div>
+		</div>
+
+		<div class="orientatie-8">
+			<div class="o-title-8">
+				<h2>Techniek</h2>
+			</div>
+			<div class="o-vakje-8">
+				<p>Onze wereld evolueert razendsnel. Techniek vormt het sleutelbegrip om de toekomst vorm te geven. Gezien de ecologische, maatschappelijke uitdagingen is het belangrijk om techniek een plaats te geven. Wij willen dat onze kinderen deze evolutie begrijpen en hierop kunnen anticiperen.</p>
+			</div>
+		</div>
+	</div>`;
     });
 
     document.querySelector(".button-3").addEventListener('click', function () {

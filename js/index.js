c335a7b1cf6f418df95baf943a1c336cfc48397f
Elouan Van Cappellen <elouan.van.cappellen@student.ehb.be>
2025-06-17 14:44:27 +0200

    fixed pos for mobile

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const styles = ['style1', 'style2', 'style3'];
-  //  const styles = ['style3']; //set style for testing
+    // const styles = ['style1']; //set style for testing
     const randomStyle = styles[Math.floor(Math.random() * styles.length)];
 
     const composition = document.getElementById('imageComposition');

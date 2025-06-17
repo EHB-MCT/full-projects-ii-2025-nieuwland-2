22ef13a6abf5e02f6c2d0651254dbab60d2ec6a8
Alyssa Marche <alyssa.marche@student.ehb.be>
2025-06-17 11:51:27 +0200

    tussentijdse commit (breedte tekst enz verandert)

--- js/visie.js
+++ js/visie.js
@@ -9,7 +9,7 @@ function filter() {
          <div class="container">
 
             <div class="pedagogisch-1">
-                <h2>Pedagogisch Project <img src="images/icon_flyer.png" class="educational-icon"></h2>
+                <h2>Pedagogisch Project</h2>
                 <img src="./images/pedagogisch_project.png" class="pedagogisch-pic-1">
                 <div class="p-vakje-1">
                     <p>Nergens voel je het kloppend hart van Brussel zo sterk als in de Marollen. Nieuwland ligt midden in deze bruisende volksbuurt en wil een plek bieden voor elke jongere tussen 2.5 en 12 jaar.</p>
@@ -17,7 +17,7 @@ function filter() {
             </div>
 
             <div class="pedagogisch-2">
-                <h2>Voel je het al kriebelen?<img src="images/icon-kriebels.png" class="kriebels-icon" /></h2>
+                <h2>Voel je het al kriebelen?</h2>
 				
                 <div class="p-vakje-2">
                     <p>Dat komt doordat we hier aan de toekomst werken. En samen kunnen we dat veel beter dan alleen. In de klas leren we met elkaar en van elkaar, met respect voor de keuze en de identiteit van iedereen. Dat vraagt een flinke portie zelfkennis, waar we samen voor zorgen.</p>
@@ -26,7 +26,7 @@ function filter() {
 
             <div class="pedagogisch-3">
                 <img src="./images/juffen_meesters.png" class="pedagogisch-pic-2">
-                <h2>En wat met al die juffen en meesters?<img src="images/icon_Juf_meesters.png" class="teachers-icon" /></h2>
+                <h2>En wat met al die juffen en meesters?</h2>
 				
                 <div class="p-vakje-3">
                     <p>Die vinden ‘samen’ ook van groot belang! Samen voor de klas in co-teaching, samen zorg dragen voor alle leerlingen in team-teaching, samen werken aan een zachte overgang tussen kleuter en lager, samen met ouders en de buurt een netwerk creëren waarin kinderen alle kansen krijgen die ze verdienen. </p>
@@ -34,7 +34,7 @@ function filter() {
             </div>
 
             <div class="pedagogisch-4">
-                <h2>Voorbereid op een complexe wereld?<img src="images/icon_complexe.png" class="complex-icon" /></h2>
+                <h2>Voorbereid op een complexe wereld?</h2>
 				
                 <div class="p-vakje-4">
                     <p>Creativiteit – in de meest brede zin van het woord - is hierop het antwoord. We willen oog hebben voor de talenten van alle leerlingen, hen helpen die te ontdekken en ten volle te ontplooien. Daarnaast stimuleren we hen om na te denken over complexe uitdagingen. De innovatieve ideeën die leerlingen opdoen zijn de motor om los te komen van stereotypen en om de wereld te bekijken vanuit verschillende perspectieven.</p>
@@ -42,7 +42,7 @@ function filter() {
             </div>
 
             <div class="pedagogisch-5">
-                <h2>Best uitdagend?<img src="images/Icon_uitdagend.png" class="challenging-icon"></h2>
+                <h2>Best uitdagend?</h2>
 				
                 <div class="p-vakje-5">
                     <p>Dat klopt! Maar we kijken positief naar de toekomst. We geloven oprecht in de leer – kracht van elk kind en vinden fouten maken noodzakelijk! De grootste uitvindingen zijn maar tot stand gekomen door veelvuldig experimenteren en fouten maken. Als school willen we hier positief op inspelen door kinderen in hun leer – kracht te begeleiden.</p>
@@ -50,7 +50,7 @@ function filter() {
             </div>
 
             <div class="pedagogisch-6">
-                <h2>Maar toch niet elk kind kan dat?<img src="images/Icon_maar.png" class="but-icon"></h2>
+                <h2>Maar toch niet elk kind kan dat?</h2>
 				
                 <img src="./images/kind_instrument.png" class="pedagogisch-pic-3">
                 <div class="p-vakje-6">
@@ -59,7 +59,7 @@ function filter() {
             </div>
 
             <div class="pedagogisch-7">
-                <h2>En zijn er dan geen regels meer op school?<img src="images/Icon_geen_regels.png" class="rules-icon"></h2>
+                <h2>En zijn er dan geen regels meer op school?</h2>
 				
                 <img src="./images/kinderen_fluo_hesjes.png" class="pedagogisch-pic-4">
                 <div class="p-vakje-7">
@@ -76,7 +76,7 @@ function filter() {
 
 		<div class="orientatie-1">
 			<!-- <div class="o-title-1"> -->
-				<h2>Mens <img src="images/icon_mens.png" class="human-icon"></h2>
+				<h2>Mens</h2>
 			<!-- </div> -->
 			<img src="./images/mens.png" class="mens">
 			<div class="o-vakje-1">
@@ -86,7 +86,7 @@ function filter() {
 
 		<div class="orientatie-2">
 			<div class="o-title-2">
-				<h2>Maatschappij <img src="images/Icon_maatschappij_ samen.png" class="society-icon"></h2>
+				<h2>Maatschappij</h2>
 			</div>
 			<img src="./images/maatschappij.png" class="maatschappij">
 			<div class="o-vakje-2">
@@ -96,7 +96,7 @@ function filter() {
 
 		<div class="orientatie-3">
 			<div class="o-title-3">
-				<h2>Ruimte: verkeer en mobiliteit <img src="images/Icon_mobiliteit.png" class="mobility-icon"></h2>
+				<h2>Ruimte: verkeer en mobiliteit</h2>
 			</div>
 			<div class="o-vakje-3">
 				<p>Door het drukke verkeer leggen we de nadruk op verkeersveiligheid, zodat kinderen zich veilig te voet en met de fiets kunnen verplaatsen. We maken optimaal gebruik van het openbaar vervoer bij uitstappen in het BHG, zodat kinderen leren zich zelfstandig te verplaatsen en hun leefwereld verbreden. Mobiliteit koppelen we aan milieueducatie, waarbij we hen bewust maken van hun ecologische impact en duurzame keuzes stimuleren.</p>
@@ -106,7 +106,7 @@ function filter() {
 		<div class="orientatie-4">
 			<img src="./images/tijd.png" class="tijd">
 			<div class="o-title-4">
-				<h2>Tijd <img src="images/icon_clock.png" alt="clock-icon"></h2>
+				<h2>Tijd</h2>
 			</div>
 			<div class="o-vakje-4">
 				<p>Om mee te kunnen in een snel veranderende samenleving, leren we kinderen hun dagelijks leven organiseren, beginnend bij tijdsbesef. Daarnaast laten we hen kennismaken met historische gebeurtenissen, zodat ze onze maatschappij beter begrijpen en bewuster over de toekomst kunnen nadenken.</p>
@@ -116,7 +116,7 @@ function filter() {
 		<div class="orientatie-5">
 			<img src="./images/levende_natuur.png" class="levende_natuur">
 			<div class="o-title-5">
-				<h2>Natuur: levende natuur <img src="images/Icon_levend.png" class="nature-icon"></h2>
+				<h2>Natuur: levende natuur</h2>
 			</div>
 			<div class="o-vakje-5">
 				<p>Onze kinderen groeien op in een verstedelijkt gebied en hebben vaak beperkte voorkennis over de natuur. Daarom laten we hen actief en ontdekkend kennismaken met diverse biotopen en organismen, binnen en buiten het BHG, zodat deze ervaringen blijven hangen.</p>
@@ -125,7 +125,7 @@ function filter() {
 
 		<div class="orientatie-6">
 			<div class="o-title-6">
-				<h2>Natuur: gezondheid <img src="images/Icon_gezondheid.png" class="health-icon"></h2>
+				<h2>Natuur: gezondheid</h2>
 			</div>
 			<div class="o-vakje-6">
 				<p>De levensomstandigheden van onze leerlingen houden verschillende gezondheidsrisico’s in (zowel fysiek als mentaal). Daarom is een doorgedreven gezondheidsbeleid op school van groot belang. Ook bewegingsopvoeding is hier een wezenlijk onderdeel van.</p>
@@ -135,7 +135,7 @@ function filter() {
 		<div class="orientatie-7">
 			<img src="./images/natuur_milieu.png" class="natuur_milieu">
 			<div class="o-title-7">
-				<h2>Natuur: Milieu <img src="images/Icon_milieu.png" class="milieu-icon"></h2>
+				<h2>Natuur: Milieu</h2>
 			</div>
 			<div class="o-vakje-7">
 				<p>Kinderen zien in hun omgeving weinig voorbeelden van respectvol gedrag naar milieu. Zo wordt er bijvoorbeeld weinig gesorteerd en slingert er veel afval in de omliggende straten. We willen dat onze leerlingen opgroeien tot burgers die de wereld beter maken en dus is het belangrijk om vanuit de school voldoende klemtoon te leggen op milieu-educatie en ecologie.</p>
@@ -144,7 +144,7 @@ function filter() {
 
 		<div class="orientatie-8">
 			<div class="o-title-8">
-				<h2>Techniek <img src="images/Icon_techniek.png" class="technology-icon"></h2>
+				<h2>Techniek</h2>
 			</div>
 			<div class="o-vakje-8">
 				<p>Onze wereld evolueert razendsnel. Techniek vormt het sleutelbegrip om de toekomst vorm te geven. Gezien de ecologische, maatschappelijke uitdagingen is het belangrijk om techniek een plaats te geven. Wij willen dat onze kinderen deze evolutie begrijpen en hierop kunnen anticiperen.</p>
@@ -160,7 +160,7 @@ function filter() {
 		<div class="lerlev-1">
 			<img src="./images/samen.png" class="samen">
 			<div class="l-title-1">
-				<h2>Samen <img src="images/Icon_maatschappij_ samen.png" class="together-icon"></h2>
+				<h2>Samen</h2>
 			</div>
 			<div class="l-vakje-1">
 				<p>Als school willen we groeien als gemeenschap en organisatie. We vertrouwen in de kracht van een team om tot die groei te komen. Samen het leerplanconcept vorm geven in de klas in co – teaching, samen reflecteren over onze ZILL aanpak in team – teaching zijn de concrete vertalingen van dit geloof in groei.  </p>
@@ -170,7 +170,7 @@ function filter() {
 
 		<div class="lerlev-2">
 			<div class="l-title-2">
-				<h2>Creatief <img src="images/Icon_createif.png" class="creativ-icon"></h2>
+				<h2>Creatief</h2>
 			</div>
 			<div class="l-vakje-2">
 				<p>Onze leerlingen worden gekenmerkt door een kleurrijk palet aan interesses, voorkeuren en talenten. Door ze via ZILL onder te dompelen in betekenisvolle contexten verruimen we hun blik op de wereld en begeleiden we ze op hun tocht naar nieuwe ontdekkingen. De brede vorming die we kinderen daarbij aanbieden krijgt een verlengstuk in onze Brede School, door activiteiten rond cultuur, welzijn en sport.  </p>
@@ -180,7 +180,7 @@ function filter() {
 
 		<div class="lerlev-3">
 			<div class="l-title-3">
-				<h2>Positief <img src="images/Icon_positief.png" class="positiv-icon"></h2>
+				<h2>Positief</h2>
 			</div>
 			<img src="./images/positief.png" class="positief">
 			<div class="l-vakje-3">
@@ -190,7 +190,7 @@ function filter() {
 
 		<div class="lerlev-4">
 			<div class="l-title-4">
-				<h2>De lat van elk kind hoog <img src="images/Icon_ladder_hoog.png" class="ladder-icon"></h2>
+				<h2>De lat van elk kind hoog</h2>
 			</div>
 			<img src="./images/lat_van_elk_kind.png" class="lat_kind">
 			<div class="l-vakje-4">
@@ -201,7 +201,7 @@ function filter() {
 		<div class="lerlev-5">
 			<img src="./images/open.png" class="open">
 			<div class="l-title-5">
-				<h2>Open <img src="images/Icon_open.png" class="open-icon"></h2>
+				<h2>Open</h2>
 			</div>
 			<div class="l-vakje-5">
 				<p>Vanuit onze christelijke inspiratie willen we samen werken aan een stimulerend en open opvoedingsklimaat, door iedereen uit te nodigen om aan ons onderwijsproject mee te werken. Andere levensbeschouwingen of religies verwelkomen we in dialoog met elkaar. Binnen ZILL willen we hier niet enkel aan werken binnen het ontwikkelveld Rooms – Katholieke Godsdienst, maar streven we naar een geïntegreerd onderwijsinhoudelijk aanbod waarbij leren leren, leren werken en leren leven hand in hand gaan.</p>
@@ -211,7 +211,7 @@ function filter() {
 		<div class="lerlev-6">
 			<img src="./images/veilig.png" class="veilig">
 			<div class="l-title-6">
-				<h2>Veilig <img src="images/Icon_veiligheid.png" class="safe-icon"></h2>
+				<h2>Veilig</h2>
 			</div>
 			<div class="l-vakje-6">
 				<p>De leerkracht is de veilige baken binnen het leerplanconcept ZILL. Hij staat aan het roer en bepaalt de koers, maar verleent ook mede eigenaarschap aan de leerlingen door samen arrangementen uit te werken. Op die manier wisselt elke leraar doorheen de arrangementen van rol: van kapitein naar mede – onderzoeker, van faciliteren tot meespelen.</p>

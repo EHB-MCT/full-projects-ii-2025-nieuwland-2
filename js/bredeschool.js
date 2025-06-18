1690adf0d9d1307c39f7535d4f80ff6bb04aa936
Alyssa Marche <alyssa.marche@student.ehb.be>
2025-06-18 14:01:48 +0200

    tussentijdse commit (decoration brede school)

--- js/bredeschool.js
+++ js/bredeschool.js
@@ -4,97 +4,95 @@ function init() {
 }
 
 function filterTitle() {
-	document.querySelector(".button-1").addEventListener('click', function () {
-		document.getElementById('title').innerHTML = `
+	document.querySelector(".button-1").addEventListener("click", function () {
+		document.getElementById("title").innerHTML = `
 			<div class="bschool-title">
 				<h1>Wat is Brede School?</h1>
 			</div>`;
-
 	});
 
-	document.querySelector(".button-2").addEventListener('click', function () {
-		document.getElementById('title').innerHTML = `
+	document.querySelector(".button-2").addEventListener("click", function () {
+		document.getElementById("title").innerHTML = `
 			<div class="hoofdst-title">
 				<h1>Hoofdstedelijke Academie</h1>
 			</div>`;
-
 	});
 
-	document.querySelector(".button-3").addEventListener('click', function () {
-		document.getElementById('title').innerHTML = `
+	document.querySelector(".button-3").addEventListener("click", function () {
+		document.getElementById("title").innerHTML = `
 			<div class="buiteling-title">
 				<h1>Buiteling</h1>
 			</div>`;
-
 	});
 
-	document.querySelector(".button-4").addEventListener('click', function () {
-		document.getElementById('title').innerHTML = `
+	document.querySelector(".button-4").addEventListener("click", function () {
+		document.getElementById("title").innerHTML = `
 		<div class="film-title">
 				<h1>Filmclub</h1>
 			</div>`;
-
 	});
-
 }
 
-
 function filterContent() {
-	document.querySelector(".button-1").addEventListener('click', function () {
-		document.getElementById('content').innerHTML = `
+	document.querySelector(".button-1").addEventListener("click", function () {
+		document.getElementById("content").innerHTML = `
       <div class="container">
+		<div class="container">
 
-		<div class="bschool-container">
-			<div class="bschool-1">
-				<h2>Wat is Brede School?</h2>
-				<div class="s-rectangle">
-				<p>Brede Scholen zijn scholen die alles inzetten op maximale ontwikkelingskansen voor kinderen en jongeren. Hoe ze dat voor elkaar krijgen? Door samen te werken met de buurt, ouders en organisaties – een écht geïntegreerde aanpak dus. </p>
+			<div class="bschool-container">
+				<div class="bschool-1">
+					
+					<h2>Wat is Brede School?</h2>
+					<div class="s-rectangle">
+					<p>Brede Scholen zijn scholen die alles inzetten op maximale ontwikkelingskansen voor kinderen en jongeren. Hoe ze dat voor elkaar krijgen? Door samen te werken met de buurt, ouders en organisaties – een écht geïntegreerde aanpak dus. </p>
+					</div>
+					<img src="images/rose.png" class="bpic">
+				</div>
+
+				<div class="bschool-2">
+					<h2>Maximale Ontwikkelingskansen</h2>
+					<div class="s-rectangle">
+					<p>Als we aan school denken, denken we vaak aan een schoolgebouw, verplichte lessen en examens. Een school kan veel meer zijn dan dat. Een school is een plaats waar jongeren zich kunnen ontwikkelen. Een Brede School werkt samen met de buurt, ouders en lokale partners in onderwijs, cultuur, sport, jeugd, kinderopvang en welzijn. Ze werken samen aan een leerrijke en fijne omgeving voor alle kinderen en jongeren, vanuit een eenzelfde pedagogische lijn.</p>
+					</div>
+				</div>
+
+				<div class="bschool-3">
+					<h2>Opgroeien als Brussels ketje</h2>
+					<div class="s-rectangle">
+					<p>Kinderen en jongeren hebben een veilige omgeving nodig om te leren, te oefenen en te ontdekken. Een Brede School is zo’n veilige omgeving. Ze biedt kinderen en jongeren maximale ontwikkelingskansen.  Elke Brede School heeft een eigen focus. Die ontstaat uit de behoeften en talenten van kinderen en jongeren en de mogelijkheden van de wijk.</p>
+					</div>
+				</div>
+
+				<div class="bschool-4">
+					<h2>Geloven in de Brede School</h2>
+					<div class="s-rectangle">
+					<p>In 2010 keurde het VGC-College de visietekst 'Brede School' goed. Dat gebeurde na een intensieve samenwerking over de grenzen van beleidsdomeinen heen.</p>
+					<p>In de praktijk realiseert de VGC deze visie op drie manieren:</p>	
+						<ol>
+						<li>- VGC subsidieert de Brusselse Brede Scholen voor wijkgerichte en lokale coördinatie.</li>
+						<li>- VGC ondersteunt Brusselse Brede Scholen inhoudelijk.</li>
+						<li>- VGC stimuleert en coördineert de Brusselbrede samenwerking en ontwikkeling van Brede Scholen.</li>
+					</ol>
+					<p>De Vlaamse Gemeenschap is een belangrijke partner van de VGC bij de realisatie van Brede School in Brussel.</p>
+					</div>
+				</div>
+
+				<div class="bschool-5">
+					<h2>Ondersteuning van de Brede Scholen in Brussel</h2>
+					<div class="s-rectangle">
+					<p><strong>Inhoudelijke ondersteuning via het Platform Brede School Brussel</strong>
+					<p>Het Platform Brede School Brussel is deel van het Onderwijscentrum Brussel (OCB). </p>
+					<p>De doelstelling? Deskundigheid bevorderen en de kwaliteit van de Brede Scholen in Brussel bewaken. Ook scholen die geen subsidies ontvangen, kunnen inhoudelijke ondersteuning krijgen. Het OCB voorziet een vormingsaanbod in groep en coaching op maat.</p></p>
+					</div>
 				</div>
 			</div>
-
-			<div class="bschool-2">
-				<h2>Maximale Ontwikkelingskansen</h2>
-				<div class="s-rectangle">
-				<p>Als we aan school denken, denken we vaak aan een schoolgebouw, verplichte lessen en examens. Een school kan veel meer zijn dan dat. Een school is een plaats waar jongeren zich kunnen ontwikkelen. Een Brede School werkt samen met de buurt, ouders en lokale partners in onderwijs, cultuur, sport, jeugd, kinderopvang en welzijn. Ze werken samen aan een leerrijke en fijne omgeving voor alle kinderen en jongeren, vanuit een eenzelfde pedagogische lijn.</p>
-				</div>
-			</div>
-
-			<div class="bschool-3">
-				<h2>Opgroeien als Brussels ketje</h2>
-				<div class="s-rectangle">
-				<p>Kinderen en jongeren hebben een veilige omgeving nodig om te leren, te oefenen en te ontdekken. Een Brede School is zo’n veilige omgeving. Ze biedt kinderen en jongeren maximale ontwikkelingskansen.  Elke Brede School heeft een eigen focus. Die ontstaat uit de behoeften en talenten van kinderen en jongeren en de mogelijkheden van de wijk.</p>
-				</div>
-			</div>
-
-			<div class="bschool-4">
-				<h2>Geloven in de Brede School</h2>
-				<div class="s-rectangle">
-				<p>In 2010 keurde het VGC-College de visietekst 'Brede School' goed. Dat gebeurde na een intensieve samenwerking over de grenzen van beleidsdomeinen heen.</p>
-				<p>In de praktijk realiseert de VGC deze visie op drie manieren:</p>	
-					<ol>
-					<li>- VGC subsidieert de Brusselse Brede Scholen voor wijkgerichte en lokale coördinatie.</li>
-					<li>- VGC ondersteunt Brusselse Brede Scholen inhoudelijk.</li>
-					<li>- VGC stimuleert en coördineert de Brusselbrede samenwerking en ontwikkeling van Brede Scholen.</li>
-				</ol>
-				<p>De Vlaamse Gemeenschap is een belangrijke partner van de VGC bij de realisatie van Brede School in Brussel.</p>
-				</div>
-			</div>
-
-			<div class="bschool-5">
-				<h2>Ondersteuning van de Brede Scholen in Brussel</h2>
-				<div class="s-rectangle">
-				<p><strong>Inhoudelijke ondersteuning via het Platform Brede School Brussel</strong>
-				<p>Het Platform Brede School Brussel is deel van het Onderwijscentrum Brussel (OCB). </p>
-				<p>De doelstelling? Deskundigheid bevorderen en de kwaliteit van de Brede Scholen in Brussel bewaken. Ook scholen die geen subsidies ontvangen, kunnen inhoudelijke ondersteuning krijgen. Het OCB voorziet een vormingsaanbod in groep en coaching op maat.</p></p>
-				</div>
-			</div>
+			<img src="images/tree_drawing.png" class="bpic2">
 		</div>
 	</div>`;
-
 	});
 
-	document.querySelector(".button-2").addEventListener('click', function () {
-		document.getElementById('content').innerHTML = `
+	document.querySelector(".button-2").addEventListener("click", function () {
+		document.getElementById("content").innerHTML = `
 	<div class="container">
 
 		<div class="hcontainer">
@@ -120,11 +118,10 @@ function filterContent() {
 				<a href="https://www.hoofdstedelijkeacademie.be/" target="_blank">Ontdek hier meer</a>
 		</div>
 	</div>`;
-
 	});
 
-	document.querySelector(".button-3").addEventListener('click', function () {
-		document.getElementById('content').innerHTML = `
+	document.querySelector(".button-3").addEventListener("click", function () {
+		document.getElementById("content").innerHTML = `
 		<div class="bu-title">
 		<h2>Welkom</h2>
 	</div>
@@ -176,8 +173,8 @@ function filterContent() {
 	</div>`;
 	});
 
-	document.querySelector(".button-4").addEventListener('click', function () {
-		document.getElementById('content').innerHTML = `
+	document.querySelector(".button-4").addEventListener("click", function () {
+		document.getElementById("content").innerHTML = `
 			<div class="main-filmcontainer">
 		<div class="filmcontainer">
 			<img src="images/Icon_film.png" class="film-icon">
